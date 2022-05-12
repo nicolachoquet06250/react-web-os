20cec96f9c9529bb50a3d2400532e42230576581
Choquet Nicolas <nicolachoquet06250@gmail.com>
2022-05-12 15:15:05 +0000

    gestion du mode admin dans le localStorage

--- src/applications/Messages/hooks/admin.js
+++ src/applications/Messages/hooks/admin.js
@@ -1,9 +1,12 @@
 import { BehaviorSubject } from "rxjs";
 import { createRxJsUseGetter } from '../../../hooks/utils/rxjs-getter';
 
-const admin = false;
+const admin = localStorage.getItem('admin') === 'true';
 const admin$ = new BehaviorSubject(admin);
 
 export const useAdmin = createRxJsUseGetter(admin, admin$);
 
-export const setAdmin = a => admin$.next(a);
+export const setAdmin = a => {
+    localStorage.setItem('admin', (a ? 'true' : 'false'))
+    admin$.next(a);
+}
